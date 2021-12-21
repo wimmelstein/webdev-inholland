3cc1c6dbd8428a55f6d32e296ae80e553088e7e7
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-12-21 11:36:25 +0100

    Added Heroku db

--- app/db.php
+++ app/db.php
@@ -11,13 +11,14 @@ class DB extends PDO
                 $dbOptions = self::getConfig();
 
                 $port = $dbOptions['port'];
+                $type = $dbOptions['type'];
                 $host = $dbOptions['hostname'];
                 $name = $dbOptions['name'];
                 $user = $dbOptions['username'];
                 $password = $dbOptions['password'];
                 $charset = 'utf8mb4';
 
-                $dsn = "mysql:host=$host;port=$port;dbname=$name;charset=$charset";
+                $dsn = "$type:host=$host;dbname=$name;charset=$charset";
 
                 self::$instance = new DB($dsn, $user, $password);
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
@@ -31,13 +32,25 @@ class DB extends PDO
 
     static function getConfig()
     {
-        return [
-
-            'hostname' => $_ENV['PHP_DB_HOST'],
-            'port' => 3306,
-            'username' => $_ENV['MYSQL_USER'],
-            'password' => $_ENV['MYSQL_ROOT_PASSWORD'],
-            'name' => $_ENV['MYSQL_DATABASE']
-        ];
+        if (getenv('DATABASE_URL') != "" {
+            $herokuDb = parse_url(getenv('DATABASE_URL'));
+            $dbopts = [
+                'type' => 'pgsql',
+                'hostname' => $herokuDb['host'],
+                'username' => $herokuDb['user'],
+                'password' => $herokuDb['pass'],
+                'name' => ltrim($herokuDb['path'], '/')
+            ];
+        } else {
+            $dbopts =
+                [
+                    'hostname' => $_ENV['PHP_DB_HOST'],
+                    'type' => 'mysql',
+                    'username' => $_ENV['MYSQL_USER'],
+                    'password' => $_ENV['MYSQL_ROOT_PASSWORD'],
+                    'name' => $_ENV['MYSQL_DATABASE']
+                ];
+        }
+        return $dbopts;
     }
 }
