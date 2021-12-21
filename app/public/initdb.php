b89e58d236d4f51d0aef09cebe8c75b5187d6745
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-12-21 16:52:57 +0100

    Debug #4

--- app/public/initdb.php
+++ app/public/initdb.php
@@ -1,5 +1,6 @@
 <?php
 include_once '../db.php';
+
 use DB;
 
 $db = DB::getInstance();
@@ -16,6 +17,13 @@ try {
 
 try {
     $db->query("INSERT INTO articles (name, price) values('Screwdriver', 10.99), ('Chandelier', 7.99);");
+} catch (PDOException $e) {
+    echo "Query failed: " . $e->getMessage();
+}
+
+try {
+    $articles = $db->query("SELECT * FROM articles;");
+    var_dump($articles);
 } catch (PDOException $e) {
     echo "Query failed: " . $e->getMessage();
 }
