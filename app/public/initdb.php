8592517885702044d3dd1e2c0b39684f591b0eb6
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-12-21 16:33:13 +0100

    Iniitalize data

---
+++ app/public/initdb.php
@@ -0,0 +1,21 @@
+<?php
+
+use DB;
+
+$db = DB::getInstance();
+
+try {
+    $db->query("CREATE TABLE if not exists ARTICLE (
+    id SERIAL PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    price numeric(10,2)
+)");
+} catch (PDOException $e) {
+    echo "Query failed: " . $e->getMessage();
+}
+
+try {
+    $db->query("INSERT INTO articles (name, price) values('Screwdriver', 10.99), ('Chandelier', 7.99);");
+} catch (PDOException $e) {
+    echo "Query failed: " . $e->getMessage();
+}
